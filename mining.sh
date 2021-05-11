291bac9d8f31f490cb89bb9aaf7f0c7450b11ff4
gibran12 <gibranalvinda29@gmail.com>
2021-05-12 02:53:43 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zergpool.com:6233 -u D7iXzGjjRx95mpMnBvmZinTpfe74Kfvr2C -p c=DOGE
