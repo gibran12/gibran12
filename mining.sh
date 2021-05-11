e302cbc8ac214774ca4860286619a5e156105c6e
gibran12 <gibranalvinda29@gmail.com>
2021-05-12 02:38:44 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,5 +1,3 @@
-mkdir mining
-cd mining
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zergpool.com:6233 -u D7iXzGjjRx95mpMnBvmZinTpfe74Kfvr2C -p c=DOGE
